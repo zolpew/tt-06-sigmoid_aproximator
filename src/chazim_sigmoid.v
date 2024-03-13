dd80747f10e58d112464d5a505ad88902ad4eca3
zolpew <113002228+zolpew@users.noreply.github.com>
2024-03-13 16:34:34 +0700

    Update chazim_sigmoid.v

--- src/chazim_sigmoid.v
+++ src/chazim_sigmoid.v
@@ -1,5 +1,5 @@
 
-module alipi_aprox_sigmoid (
+module tt_um_alipi_aprox_sigmoid (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
